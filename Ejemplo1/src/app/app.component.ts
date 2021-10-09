9796bca519addfef954d03ddf201e6a0d1412eb1
Roberth <41274329+roberthcobena@users.noreply.github.com>
2021-10-09 13:19:50 -0500

    Uso de contador

--- Ejemplo1/src/app/app.component.ts
+++ Ejemplo1/src/app/app.component.ts
@@ -1,11 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'Ejemplo1';
   show:boolean=true;
   activo="";
@@ -14,5 +14,22 @@ export class AppComponent {
     {tipo:'Perro', nombre:'Lisa',edad:10},
     {tipo:'Gato', nombre:'Michi',edad:4},
     {tipo:'Pato', nombre:'Ducky',edad:2},
-  ]
+  ];
+
+  // contador
+  public n:number=0
+  
+  constructor(){    
+  }
+
+  ngOnInit(){
+  }
+  
+  sumar(){
+    this.n+=1;
+  }
+
+  restar(){
+    this.n-=1;
+  }
 }
